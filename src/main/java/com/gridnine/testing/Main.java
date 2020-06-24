cda25c1d6546aac8d7e2fc053585819d15b1b97e
MukhametdinovM <m.mukhametdinov@pflb.ru>
2020-06-25 01:07:23 +0300

    Final

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -5,12 +5,15 @@ import java.time.LocalDateTime;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("--- Arrival date before departure date ---");
         FlightBuilder.createFlights().stream()
                                      .filter(FlightPredicates.isToEarlyDeparture(LocalDateTime.now()))
                                      .forEachOrdered(System.out::println);
+        System.out.println("--- Departure before a specified point in time ---");
         FlightBuilder.createFlights().stream()
                                      .filter(FlightPredicates.isToEarlySegments(LocalDateTime.now()))
                                      .forEachOrdered(System.out::println);
+        System.out.println("--- Total time spent on earth for more than two hours ---");
         FlightBuilder.createFlights().stream()
                                      .filter(FlightPredicates.moreThanTwoHoursTotal())
                                      .forEachOrdered(System.out::println);
